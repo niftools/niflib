2f096c53b0539f24adcae1d98dfd47f3e9e882ff
Amorilia <amorilia@users.sourceforge.net>
2005-12-21 23:46:22 +0000

    Removed the GNU buildsystem; SCons supersedes it.

--- go.bat
+++
@@ -1,4 +0,0 @@
-rem Windows Niflib Python wrapper compilation script (using the Visual C++ Toolkit & SWIG 1.3.27)
-"\Program Files\swigwin-1.3.27\swig.exe" -c++ -python -o py_wrap.cpp pyniflib.i
-cl /LD /EHsc /I"\Program Files\Python24\include" /I. /Fe"_niflib.dll" niflib.cpp docsys_extract.cpp NIF_Blocks.cpp NIF_IO.cpp nif_math.cpp py_wrap.cpp "\Program Files\Python24\libs\python24.lib"
-rem cl /EHsc /I. /Fe"test.exe" test.cpp niflib.cpp docsys_extract.cpp NIF_Blocks.cpp NIF_IO.cpp nif_math.cpp
