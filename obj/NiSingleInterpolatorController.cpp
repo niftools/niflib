7fcaa8457c348784c82fe441bba6d21973452d92
Shon Ferguson <shonferg@users.sourceforge.net>
2006-05-28 03:37:34 +0000

    Updated license text in remaining files.  Short text in all files except niflib.h, and year updated to 2006.

--- obj/NiSingleInterpolatorController.cpp
+++ obj/NiSingleInterpolatorController.cpp
@@ -5,25 +5,25 @@ All rights reserved.  Please see niflib.h for licence. */
 #include "AInterpolator.h"
 
 //Definition of TYPE constant
-const Type NiSingleInterpolatorController::TYPE("NiSingleInterpolatorController", &A_SINGLE_INTERPOLATOR_CONTROLLER_PARENT::TYPE );
+const Type NiSingleInterpolatorController::TYPE("NiSingleInterpolatorController", &NI_SINGLE_INTERPOLATOR_CONTROLLER_PARENT::TYPE );
 
-NiSingleInterpolatorController::NiSingleInterpolatorController() A_SINGLE_INTERPOLATOR_CONTROLLER_CONSTRUCT {}
+NiSingleInterpolatorController::NiSingleInterpolatorController() NI_SINGLE_INTERPOLATOR_CONTROLLER_CONSTRUCT {}
 
 NiSingleInterpolatorController::~NiSingleInterpolatorController() {}
 
 void NiSingleInterpolatorController::Read( istream& in, list<uint> link_stack, unsigned int version ) {
-	A_SINGLE_INTERPOLATOR_CONTROLLER_READ
+	NI_SINGLE_INTERPOLATOR_CONTROLLER_READ
 }
 
 void NiSingleInterpolatorController::Write( ostream& out, map<NiObjectRef,uint> link_map, unsigned int version ) const {
-	A_SINGLE_INTERPOLATOR_CONTROLLER_WRITE
+	NI_SINGLE_INTERPOLATOR_CONTROLLER_WRITE
 }
 
 string NiSingleInterpolatorController::asString( bool verbose ) const {
-	A_SINGLE_INTERPOLATOR_CONTROLLER_STRING
+	NI_SINGLE_INTERPOLATOR_CONTROLLER_STRING
 }
 
 void NiSingleInterpolatorController::FixLinks( const vector<NiObjectRef> & objects, list<uint> link_stack, unsigned int version ) {
-	A_SINGLE_INTERPOLATOR_CONTROLLER_FIXLINKS
+	NI_SINGLE_INTERPOLATOR_CONTROLLER_FIXLINKS
 }
 
--- obj/NiSingleInterpolatorController.h
+++ obj/NiSingleInterpolatorController.h
@@ -1,11 +1,11 @@
 /* Copyright (c) 2006, NIF File Format Library and Tools
 All rights reserved.  Please see niflib.h for licence. */
 
-#ifndef _ASINGLEINTERPOLATORCONTROLLER_H_
-#define _ASINGLEINTERPOLATORCONTROLLER_H_
+#ifndef _NISINGLEINTERPOLATORCONTROLLER_H_
+#define _NISINGLEINTERPOLATORCONTROLLER_H_
 
 #include "xml_extract.h"
-#include A_SINGLE_INTERPOLATOR_CONTROLLER_INCLUDE
+#include NI_SINGLE_INTERPOLATOR_CONTROLLER_INCLUDE
 
 /*
  * NiSingleInterpolatorController
@@ -14,7 +14,7 @@ All rights reserved.  Please see niflib.h for licence. */
 class NiSingleInterpolatorController;
 typedef Ref<NiSingleInterpolatorController> NiSingleInterpolatorControllerRef;
 
-class NiSingleInterpolatorController : public A_SINGLE_INTERPOLATOR_CONTROLLER_PARENT {
+class NiSingleInterpolatorController : public NI_SINGLE_INTERPOLATOR_CONTROLLER_PARENT {
 public:
 	NiSingleInterpolatorController();
 	~NiSingleInterpolatorController();
@@ -25,6 +25,6 @@ public:
 	virtual string asString( bool verbose = false ) const;
 	virtual void FixLinks( const vector<NiObjectRef> & objects, list<uint> link_stack, unsigned int version );
 private:
-	A_SINGLE_INTERPOLATOR_CONTROLLER_MEMBERS
+	NI_SINGLE_INTERPOLATOR_CONTROLLER_MEMBERS
 };
 #endif
