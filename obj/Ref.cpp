6f0567242e23759b26b7f1403f48082528f439e6
Shon Ferguson <shonferg@users.sourceforge.net>
2006-05-28 03:31:14 +0000

    Implemented Ref member functions.

--- obj/Ref.cpp
+++ obj/Ref.cpp
@@ -8,32 +8,68 @@ Ref<T>::Ref( T * object = NULL ) : _object(object) {}
 
 template <class T>
 Ref<T>::Ref(const Ref & ref_to_copy ) {
-
+	_object = ref_to_copy._object;
+	//If object isn't null, increment reference count
+	if ( _object != NULL ) {
+		_object->AddRef();
+	}
 }
 
 template <class T>
 Ref<T>::operator T*() const {
-
+	return _object;
 }
 
 template <class T>
 T& Ref<T>::operator*() const {
-
+	return _object;
 }
 
 template <class T>
 T* Ref<T>::operator->() const {
-
+	return _object;
 }
 
 template <class T>
 Ref<T> & Ref<T>::operator=( T * object ) {
+	//Check if referenced objects are already the same
+	if ( _object == object ) {
+		return this; //Do nothing
+	}
 
+	//Decriment reference count on previously referenced object, if any
+	if ( _object != NULL ) {
+		_object->SubtractRef();
+	}
+
+	//Change reference to new object
+	_object = object;
+
+	//Increment reerence count on new object if it is not NULL
+	if ( _object != NULL ) {
+		_object->AddRef();
+	}
 }
 
 template <class T>
 Ref<T> & Ref<T>::operator=( const Ref & ref ) {
+	//Check if referenced objects are already the same
+	if ( _object == ref._object ) {
+		return this; //Do nothing
+	}
 
+	//Decriment reference count on previously referenced object, if any
+	if ( _object != NULL ) {
+		_object->SubtractRef();
+	}
+
+	//Change reference to new object
+	_object = ref._object;
+
+	//Increment reerence count on new object if it is not NULL
+	if ( _object != NULL ) {
+		_object->AddRef();
+	}
 }
 
 template <class T>
@@ -43,20 +79,24 @@ bool Ref<T>::operator<(const Ref & ref) const {
 
 template <class T>
 bool Ref<T>::operator==(T * object) const {
-
+	//Compare pointer values of referenced objects
+	return ( _object == object );
 }
 
 template <class T>
 bool Ref<T>::operator!=(T * object) const {
-
+	//Compare pointer values of referenced objects
+	return ( _object != object );
 }
 
 template <class T>
 bool Ref<T>::operator==(const Ref & ref) const {
-
+	//Compare pointer values of referenced objects
+	return ( _object == ref._object );
 }
 
 template <class T>
 bool Ref<T>::operator!=(const Ref & ref) const {
-
+	//Compare pointer values of referenced objects
+	return ( _object != ref._object );
 }
