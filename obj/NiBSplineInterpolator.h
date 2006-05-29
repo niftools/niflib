52a656252e0cc9322883bf7be24a9d890774a41a
Amorilia <amorilia@users.sourceforge.net>
2006-05-29 16:53:00 +0000

    Added two missing files. Updated SConstruct.

---
+++ obj/NiBSplineInterpolator.h
@@ -0,0 +1,32 @@
+/* Copyright (c) 2006, NIF File Format Library and Tools
+All rights reserved.  Please see niflib.h for licence. */
+
+#ifndef _NIBSPLINEINTERPOLATOR_H_
+#define _NIBSPLINEINTERPOLATOR_H_
+
+#include "xml_extract.h"
+#include NI_B_SPLINE_INTERPOLATOR_INCLUDE
+
+/*
+ * NiBSplineInterpolator
+ */
+
+class NiBSplineInterpolator;
+typedef Ref<NiBSplineInterpolator> NiBSplineInterpolatorRef;
+
+class NiBSplineInterpolator : public NI_B_SPLINE_INTERPOLATOR_PARENT {
+public:
+  NiBSplineInterpolator();
+  ~NiBSplineInterpolator();
+  //Run-Time Type Information
+  static const Type TYPE;
+  virtual void Read( istream& in, list<uint> & link_stack, unsigned int version );
+  virtual void Write( ostream& out, map<NiObjectRef,uint> link_map, unsigned int version ) const;
+  virtual string asString( bool verbose = false ) const;
+  virtual void FixLinks( const vector<NiObjectRef> & objects, list<uint> & link_stack, unsigned int version );
+  virtual const Type & GetType() const;
+protected:
+  NI_B_SPLINE_INTERPOLATOR_MEMBERS
+};
+
+#endif
