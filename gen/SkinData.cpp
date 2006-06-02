f6aa27941a56eb4d9b364c72fc77b33a3b7ccd89
Shon Ferguson <shonferg@users.sourceforge.net>
2006-06-02 05:25:10 +0000

    Fixed so Niflyze compiles too.

---
+++ gen/SkinData.cpp
@@ -0,0 +1,10 @@
+/* Copyright (c) 2006, NIF File Format Library and Tools
+All rights reserved.  Please see niflib.h for licence. */
+
+#include "SkinData.h"
+
+//Constructor
+SkinData::SkinData() : scale(0.0f), numVertices((ushort)0) {};
+
+//Destructor
+SkinData::~SkinData() {};
