f7a84c064f2346ab9e53f44cb70bffe4c348abd5
Shon Ferguson <shonferg@users.sourceforge.net>
2006-06-06 05:08:46 +0000

    Implemented functions for NiTriStripsData.

--- obj/NiTriStripsData.cpp
+++ obj/NiTriStripsData.cpp
@@ -34,3 +34,73 @@ const Type & NiTriStripsData::GetType() const {
 	return TYPE;
 };
 
+ushort NiTriStripsData::GetStripCount() const {
+	return ushort(points.size());
+}
+
+void NiTriStripsData::SetStripCount(int n) {
+	points.resize( n );
+
+	//Recalculate Triangle Count
+	numTriangles = CalcTriangleCount();
+}
+
+//Getters
+vector<ushort> NiTriStripsData::GetStrip( int index ) const {
+	return points[index];
+}
+
+vector<Triangle> NiTriStripsData::GetTriangles() const {
+
+	//Create a vector to hold the triangles
+	vector<Triangle> triangles( numTriangles );
+	int n = 0; // Current triangle
+
+	//Cycle through all strips
+	vector< vector<ushort> >::const_iterator it;
+	for (it = points.begin(); it != points.end(); ++it ) {
+		//The first three values in the strip are the first triangle
+		triangles[n].Set( (*it)[0], (*it)[1], (*it)[2] );
+
+		//Move to the next triangle
+		++n;
+
+		//The remaining triangles use the previous two indices as their first two indices.
+		for( uint i = 3; i < it->size(); ++i ) {
+			//Odd numbered triangles need to be reversed to keep the vertices in counter-clockwise order
+			if ( i % 2 == 0 ) {
+				//cout << (*it)[i - 2] << ", " << (*it)[i - 1] << ", " << (*it)[i] << endl;
+				triangles[n].Set( (*it)[i - 2], (*it)[i - 1], (*it)[i] );
+			} else {
+				//cout << (*it)[i] << ", " << (*it)[i - 1] << ", " << (*it)[i - 2] << endl;
+				triangles[n].Set( (*it)[i], (*it)[i - 1], (*it)[i - 2] );
+			}
+
+			//Move to the next triangle
+			++n;
+		}
+	}
+
+	return triangles;
+}
+
+//Setter
+void NiTriStripsData::SetStrip( int index, const vector<ushort> & in ) {
+	points[index] = in;
+
+	//Recalculate Triangle Count
+	numTriangles = CalcTriangleCount();
+}
+
+ushort NiTriStripsData::CalcTriangleCount() const {
+
+	//Calculate number of triangles
+	//Sum of length of each strip - 2
+	ushort numTriangles = 0;
+	for ( uint i = 0; i < points.size(); ++i ) {
+		numTriangles += ushort(points[i].size() - 2);
+	}
+
+	return numTriangles;
+}
+
--- obj/NiTriStripsData.h
+++ obj/NiTriStripsData.h
@@ -27,6 +27,47 @@ public:
 	virtual void FixLinks( const vector<NiObjectRef> & objects, list<uint> & link_stack, unsigned int version, unsigned int user_version );
 	virtual list<NiObjectRef> GetRefs() const;
 	virtual const Type & GetType() const;
+
+	//--Counts--//
+
+	/*! Used to get the number of triangle strips that this mesh is divided into.
+	 * \return The number of triangle strips used by this mesh.
+	 * \sa NiTriStripData::SetStripCount
+	 */
+	ushort GetStripCount() const;
+
+	/*! Used to resize the triangle strips array.  If the new size is smaller, strips at the end of the array will be deleted.
+	 * \param n The new size of the triangle strips array.
+	 * \sa NiTriStripData::GetStripCount
+	 */
+	void SetStripCount(int n);
+	
+	//--Getters--//
+
+	/*! Used to retrieve all the triangles from a specific triangle strip.
+	 * \param index The index of the triangle strip to retrieve the triangles from.  This is a zero-based index which must be a positive number less than that returned by NiTriStripsData::GetStripCount.
+	 * \return A vector containing all the triangle faces from the triangle strip specified by index.
+	 * \sa NiTriStripData::SetStrip, NiTriStripData::GetTriangles
+	 */
+	vector<ushort> GetStrip( int index ) const;
+
+	/*! This is a conveniance function which returns all triangle faces in all triangle strips that make up this mesh.  It is similar to the ITriShapeData::GetTriangles function.
+	 * \return A vector containing all the triangle faces from all the triangle strips that make up this mesh.
+	 * \sa NiTriStripData::GetTriangles, NiTriStripData::GetStrip, NiTriStripData::SetStrip
+	 */
+	vector<Triangle> GetTriangles() const;
+
+	//--Setter--/
+
+	/*! Used to set the triangle face data in a specific triangle strip.
+	 * \param index The index of the triangle strip to set the face data for.  This is a zero-based index which must be a positive number less than that returned by NiTriStripsData::GetStripCount.
+	 * \sa NiTriStripData::GetStrip, NiTriStripData::GetTriangles
+	 */
+	void SetStrip( int index, const vector<ushort> & in );
+
+private:
+	ushort CalcTriangleCount() const;
+
 protected:
 	NI_TRI_STRIPS_DATA_MEMBERS
 };
