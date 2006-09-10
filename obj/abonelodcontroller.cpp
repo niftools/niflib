e8d1beb981580e973d7c555db45d10600927149f
Tazpn <tazpn@users.sourceforge.net>
2006-09-10 01:14:15 +0000

    Strangely this seems to be missing from niflib according to my SVN project.

---
+++ obj/abonelodcontroller.cpp
@@ -0,0 +1,97 @@
+/* Copyright (c) 2006, NIF File Format Library and Tools
+All rights reserved.  Please see niflib.h for licence. */
+
+#include "ABoneLODController.h"
+#include "../gen/NodeGroup.h"
+#include "NiNode.h"
+#include <algorithm>
+using namespace Niflib;
+
+//Definition of TYPE constant
+const Type ABoneLODController::TYPE("ABoneLODController", &A_BONE_L_O_D_CONTROLLER_PARENT::TypeConst() );
+
+ABoneLODController::ABoneLODController() A_BONE_L_O_D_CONTROLLER_CONSTRUCT {}
+
+ABoneLODController::~ABoneLODController() {}
+
+void ABoneLODController::Read( istream& in, list<uint> & link_stack, unsigned int version, unsigned int user_version ) {
+	A_BONE_L_O_D_CONTROLLER_READ
+}
+
+void ABoneLODController::Write( ostream& out, map<NiObjectRef,uint> link_map, unsigned int version, unsigned int user_version ) const {
+	A_BONE_L_O_D_CONTROLLER_WRITE
+}
+
+string ABoneLODController::asString( bool verbose ) const {
+	A_BONE_L_O_D_CONTROLLER_STRING
+}
+
+void ABoneLODController::FixLinks( const vector<NiObjectRef> & objects, list<uint> & link_stack, unsigned int version, unsigned int user_version ) {
+	A_BONE_L_O_D_CONTROLLER_FIXLINKS
+}
+
+list<NiObjectRef> ABoneLODController::GetRefs() const {
+	A_BONE_L_O_D_CONTROLLER_GETREFS
+}
+
+const Type & ABoneLODController::GetType() const {
+	return TYPE;
+};
+
+/*!
+ * A list of node groups (each group a sequence of bones?).
+ */
+int ABoneLODController::GetNodeGroupCount() const {
+   return int(nodeGroups.size());
+}
+
+vector<Ref<NiNode> > ABoneLODController::GetNodeGroup( int index ) const {
+   if (index < 0 || index >= int(nodeGroups.size()) ) {
+      throw runtime_error("Invalid index referenced.");
+   }
+   return nodeGroups[index].nodes;   
+}
+
+void ABoneLODController::AddNodeToGroup( int index, Ref<NiNode> node ) {
+   while (index >= int(nodeGroups.size()))
+      nodeGroups.insert(nodeGroups.end(), NodeGroup() );
+   numNodeGroups2 = nodeGroups.size();
+
+   vector<NiNodeRef>& nodes = nodeGroups[index].nodes;
+   vector<NiNodeRef>::iterator itr = std::find(nodes.begin(), nodes.end(), node);
+   if (itr == nodes.end())
+      nodes.push_back(node);
+}
+
+void ABoneLODController::RemoveNodeFromGroup( int index, Ref<NiNode> node ) {
+   if (index < 0 || index >= int(nodeGroups.size()) ) {
+      throw runtime_error("Invalid index referenced.");
+   }
+   vector<NiNodeRef>& nodes = nodeGroups[index].nodes;
+   vector<NiNodeRef>::iterator itr = std::find(nodes.begin(), nodes.end(), node);
+   if (itr == nodes.end())
+      return;
+   nodes.erase(itr);
+}
+
+void ABoneLODController::SetNodeGroup( int index, const vector<Ref<NiNode> >& group ) {
+   while (index >= int(nodeGroups.size()))
+      nodeGroups.insert(nodeGroups.end(), NodeGroup() );
+   numNodeGroups2 = nodeGroups.size();
+   nodeGroups[index].nodes.assign(group.begin(), group.end());
+}
+
+void ABoneLODController::RemoveNodeGroup( int index ) {
+   if (index < 0 || index >= int(nodeGroups.size()) ) {
+      throw runtime_error("Invalid index referenced.");
+   }
+   vector<NodeGroup>::iterator itr = nodeGroups.begin();
+   std::advance(itr, index);
+   nodeGroups.erase(itr);
+   numNodeGroups2 = nodeGroups.size();
+}
+
+void ABoneLODController::ClearNodeGroups() {
+   nodeGroups.clear();
+   numNodeGroups2 = nodeGroups.size();
+}
