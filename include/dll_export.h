215e90cc94cc9560b1ee6370572fdc56d35be000
Amorilia <amorilia@users.sourceforge.net>
2007-03-30 09:04:25 +0000

    niflib: * fixed detection of visibility=hidden support (gcc, linux) * compilation will give error message if visibility=hidden is not detected

--- include/dll_export.h
+++ include/dll_export.h
@@ -4,23 +4,35 @@ All rights reserved.  Please see niflib.h for licence. */
 #ifndef _DLL_EXPORT_H_
 #define _DLL_EXPORT_H_
 
+// detect visibility support for linux compilers
+#if (__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+#  ifndef GCC_HASCLASSVISIBILITY
+#    define GCC_HASCLASSVISIBILITY
+#  endif
+#endif
+
+// shared library: expose NIFLIB_API objects, hide NIFLIB_HIDDEN objects
 #ifdef USE_NIFLIB_DLL
-	#ifdef _MSC_VER
+	// building swig wrapper
+	#if defined(SWIG)
+		#define NIFLIB_API
+		#define NIFLIB_HIDDEN
+	// building shared library (windows)
+	#elif defined(_WIN32) || defined(__WIN32__) || defined(_MSC_VER) || defined(__CYGWIN__)
 		#ifdef BUILDING_NIFLIB_DLL
 			#define NIFLIB_API __declspec(dllexport)
 		#else
 			#define NIFLIB_API __declspec(dllimport)
 		#endif
 		#define NIFLIB_HIDDEN
+	// building shared library (linux)
+	#elif defined(__GNUC__) && defined(GCC_HASCLASSVISIBILITY)
+		#define NIFLIB_API __attribute__ ((visibility("default")))
+		#define NIFLIB_HIDDEN __attribute__ ((visibility("hidden")))
 	#else
-		#ifdef HAVE_GCCVISIBILITYPATCH
-			#define NIFLIB_API __attribute__ ((visibility("default")))
-			#define NIFLIB_HIDDEN __attribute__ ((visibility("hidden")))
-		#else
-			#define NIFLIB_API
-			#define NIFLIB_HIDDEN
-		#endif
-	#endif 
+		#error __attribute__ ((visibility("hidden"))) support required, but not detected (see gcc.gnu.org/wiki/Visibility)
+	#endif
+// static library: nothing to hide
 #else
 	#define NIFLIB_API
 	#define NIFLIB_HIDDEN
