1622bbd3043c4881b6c7af4ea1851a79980b7a01
Amorilia <amorilia@users.sourceforge.net>
2012-10-07 22:45:39 +0100

    Also use CPack for source release.

--- make-release.sh
+++
@@ -1,7 +0,0 @@
-VERSION=`cat VERSION`
-rm ../niflib-${VERSION}-src.zip
-git submodule update --recursive
-git clean -xfd
-git submodule foreach --recursive git clean -xfd
-doxygen
-find . -type f -not -wholename "*.git*" -and -not -name ".project" | xargs zip ../niflib-${VERSION}-src.zip
